5fb2259ee7f9208e366290463c15ce05133139cd
Ben Osborne <ben@osbornesupremacy.com>
2026-05-03 16:14:34 -0500

    Add separate Lambda permissions for blue and green API Gateway stages

--- iac/modules/api-gateway-integration/gateway-integration.tf
+++ iac/modules/api-gateway-integration/gateway-integration.tf
@@ -9,14 +9,21 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   content_handling = "CONVERT_TO_TEXT"
 }
 
-# Allow API Gateway to invoke any version, alias, or $LATEST of the Lambda function
-resource "aws_lambda_permission" "api_gateway_invoke" {
+
+resource "aws_lambda_permission" "api_gateway_invoke_blue" {
   statement_id  = "AllowAPIGatewayInvoke-${var.gateway_http_method}-${var.gateway_rest_api_id}"
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_function_arn
   principal     = "apigateway.amazonaws.com"
-  # arn:aws:execute-api:{region}:{account}:{api-id}/*/{method}/{resource}
-  # The :* qualifier grants permission for all versions, aliases, and $LATEST
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${var.gateway_rest_api_id}/*/*"
-  qualifier  = null
+  source_arn    = "arn:aws:execute-api:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${var.gateway_rest_api_id}/*/*"
+  qualifier     = "blue"
+}
+
+resource "aws_lambda_permission" "api_gateway_invoke_green" {
+  statement_id  = "AllowAPIGatewayInvoke-${var.gateway_http_method}-${var.gateway_rest_api_id}"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${var.gateway_rest_api_id}/*/*"
+  qualifier     = "green"
 }
