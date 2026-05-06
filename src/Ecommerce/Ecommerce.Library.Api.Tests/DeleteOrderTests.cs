e4f3c3e90b2f7dadd56ce4a5302b6caef6d64be2
Ben Osborne <ben@osbornesupremacy.com>
2026-05-06 17:11:09 -0500

    Add DeleteOrderTests and GetOrderTests for order deletion and retrieval functionality

---
+++ src/Ecommerce/Ecommerce.Library.Api.Tests/DeleteOrderTests.cs
@@ -0,0 +1,49 @@
+namespace Ecommerce.Library.Api.Tests;
+
+public class DeleteOrderTests(ApiTestsFixture fixture) : IClassFixture<ApiTestsFixture>
+{
+    [Fact]
+    public async Task DeleteOrder_GivenExistingOrder_ReturnsNoContent()
+    {
+        // arrange
+        var order = await fixture.CreateRandomOrderAsync();
+        var httpClient = fixture.GetHttpClient();
+
+        var deleteRequest = new DeleteOrderRequest
+        {
+            CustomerId = order.CustomerId,
+            OrderId = order.OrderId
+        };
+
+        // act
+        var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Delete, "v1/order")
+        {
+            Content = JsonContent.Create(deleteRequest)
+        });
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task DeleteOrder_GivenNonExistentOrder_ReturnsNoContent()
+    {
+        // arrange
+        var httpClient = fixture.GetHttpClient();
+
+        var deleteRequest = new DeleteOrderRequest
+        {
+            CustomerId = Guid.CreateVersion7().ToString(),
+            OrderId = Guid.NewGuid()
+        };
+
+        // act
+        var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Delete, "v1/order")
+        {
+            Content = JsonContent.Create(deleteRequest)
+        });
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+}
---
+++ src/Ecommerce/Ecommerce.Library.Api.Tests/GetOrderTests.cs
@@ -0,0 +1,37 @@
+namespace Ecommerce.Library.Api.Tests;
+
+public class GetOrderTests(ApiTestsFixture fixture) : IClassFixture<ApiTestsFixture>
+{
+    [Fact]
+    public async Task GetOrder_GivenExistingOrder_ReturnsSuccess()
+    {
+        // arrange
+        var order = await fixture.CreateRandomOrderAsync();
+        var httpClient = fixture.GetHttpClient();
+
+        // act
+        var response = await httpClient.GetAsync($"v1/order/{order.CustomerId}/{order.OrderId}");
+        var responseBody = await response.Content.ReadFromJsonAsync<GetOrderResponse>();
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        responseBody.Should().NotBeNull();
+        responseBody.OrderId.Should().Be(order.OrderId);
+        responseBody.CustomerId.Should().Be(order.CustomerId);
+    }
+
+    [Fact]
+    public async Task GetOrder_GivenNonExistentOrder_ReturnsNotFound()
+    {
+        // arrange
+        var httpClient = fixture.GetHttpClient();
+        var customerId = Guid.CreateVersion7().ToString();
+        var orderId = Guid.NewGuid();
+
+        // act
+        var response = await httpClient.GetAsync($"v1/order/{customerId}/{orderId}");
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
